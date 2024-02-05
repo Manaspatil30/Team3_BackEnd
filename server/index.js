b824a7326281e00b8066e091c86fe86b6ace0d45
Max Michael Mendonca <72253380+maxmendoncaa@users.noreply.github.com>
2024-02-05 11:41:44 +0000

    created node server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(3001, (req, res)=>{
+    console.log("Server is running at port 3001");
+})
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: "root",
+    password: "",
+    database: "uni_kart"
+})
+
+app.get('/',(req,res)=>{
+    const selectQuery = "SELECT * from userregistration"
+    db.query(selectQuery,(err, result)=>{
+        if(err) {console.log(err)};
+        res.send(result)
+    })
+})
