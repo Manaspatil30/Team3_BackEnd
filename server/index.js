3ce0f6572e50d5c2c732e0dc3f1e6f9dfe8c474b
Max Michael Mendonca <72253380+maxmendoncaa@users.noreply.github.com>
2024-02-06 20:24:31 +0000

    Temporary routes to DB

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const db = mysql.createPool({
     host: 'localhost',
     user: "root",
     password: "",
-    database: "uni_kart"
+    database: "unikartdatabase"
 })
 
 app.get('/',(req,res)=>{
@@ -25,3 +25,56 @@ app.get('/',(req,res)=>{
         res.send(result)
     })
 })
+
+app.get('/user/:id', (req, res) => {
+    const userId = 1;
+    const selectQuery = "SELECT * FROM userregistration WHERE user_id = ?";
+    db.query(selectQuery, [userId], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.post('/user/add', (req, res) => {
+    const { first_name, last_name, phone_number, email, address, membership, start_date, end_date } = req.body;
+    const insertQuery = "INSERT INTO userregistration (first_name, last_name, phone_number, email, address, membership, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    db.query(insertQuery, [first_name, last_name, phone_number, email, address, membership, start_date, end_date], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Failed to add user");
+        } else {
+            res.status(201).send("User added successfully");
+        }
+    });
+});
+
+app.put('/user/update/:id', (req, res) => {
+    const userId = req.params.id;
+    const { first_name, last_name, phone_number, email, address, membership, start_date, end_date } = req.body;
+    const updateQuery = "UPDATE userregistration SET first_name=?, last_name=?, phone_number=?, email=?, address=?, membership=?, start_date=?, end_date=? WHERE user_id=?";
+    db.query(updateQuery, [first_name, last_name, phone_number, email, address, membership, start_date, end_date, userId], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Failed to update user information");
+        } else {
+            res.status(200).send("User information updated successfully");
+        }
+    });
+});
+
+app.delete('/user/delete/:id', (req, res) => {
+    const userId = req.params.id;
+    const deleteQuery = "DELETE FROM userregistration WHERE user_id=?";
+    db.query(deleteQuery, [userId], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Failed to delete user");
+        } else {
+            res.status(200).send("User deleted successfully");
+        }
+    });
+});
