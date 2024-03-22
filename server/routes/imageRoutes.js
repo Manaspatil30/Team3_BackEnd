0f2f1029887fa7febdd6269e1b086174936df078
Max Michael Mendonca <72253380+maxmendoncaa@users.noreply.github.com>
2024-03-22 01:25:44 +0000

    image test

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -11,8 +11,20 @@ cloudinary.config({
     api_key: '116233414617767',
     api_secret: 'QGxpcw5tvz3s3ZdoBlWA89qgtus'
 });
+router.get('/uploadi', (req, res) => {
+    const rseq = req.body;
+    cloudinary.v2.uploader.upload("a.jpeg")
+      .then(result => {
+        console.log(result);
+        res.status(200).json({ success: true, result });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ success: false, error: error.message });
+      });
+  });
 
-router.post('/upload',  (req, res) => {
+router.put('/upload',  (req, res) => {
   try {
     
     // const image=req.body.image;
