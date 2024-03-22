4a36369310b808abd57e8dd5c2ba040c53feb413
William Wan <122927183+WillWan728@users.noreply.github.com>
2024-03-22 15:57:49 +0000

    Changes to get product and store info

--- server/routes/priceRoutes.js
+++ server/routes/priceRoutes.js
@@ -28,24 +28,20 @@ router.get('/products/:productId', (req, res) => {
   
       // If the product is found, execute the store query
       if (productResult.length > 0) {
+        const product = productResult[0];
+  
         db.query(storeQuery, [productId], (err, storeResult) => {
           if (err) throw err;
   
-          const product = productResult[0];
-          const stores = storeResult;
-  
-          // Prepare the response object
-          const response = {
-            productId: product.product_id,
-            productName: product.product_name,
+          // Prepare the response array
+          const response = storeResult.map(store => ({
+            Product_id: product.product_id,
+            productname: product.product_name,
             description: product.description,
             category: product.category,
             quantity: product.quantity,
-            stores: stores.map(store => ({
-              price: store.price,
-              storeName: store.store_name
-            }))
-          };
+            storeName: store.store_name
+          }));
   
           res.json(response);
         });
@@ -55,38 +51,5 @@ router.get('/products/:productId', (req, res) => {
       }
     });
   });
-
-// router.get('/price-comparison/:productId', (req, res) => {
-//   const productId = req.params.productId;
-
-//   // Query to fetch prices and product details from storeproducts and products tables
-//   const selectQuery = `
-//     SELECT
-//       p.product_id,
-//       p.product_name,
-//       p.description,
-//       p.category,
-//       sp.price,
-//       sp.quantity,
-//       s.store_name
-//     FROM
-//       storeproducts sp
-//     JOIN
-//       product p ON sp.product_id = p.product_id
-//     JOIN
-//       stores s ON sp.store_id = s.store_id
-//     WHERE
-//       sp.product_id = ?;
-//   `;
-
-//   db.query(selectQuery, [productId], (err, result) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     } else {
-//       res.json(result);
-//     }
-//   });
-// });
 
 export default router;
