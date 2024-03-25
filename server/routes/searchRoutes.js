4f005739f85a38e4a8c8aff7893e1ed477f53b3c
William Wan <122927183+WillWan728@users.noreply.github.com>
2024-03-25 10:47:47 +0000

    fixed product search

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -4,15 +4,20 @@ import db from "../config/db.js";
 const router = express.Router();
 
 router.get('/products/search', (req, res) => {
-    const { search } = req.query; 
-    const searchQuery = "SELECT * FROM product WHERE product_name LIKE ?";
+    const { search } = req.query;
+    const searchQuery = `
+      SELECT p.*, sp.store_id, sp.price
+      FROM product p
+      JOIN storeproducts sp ON p.product_id = sp.product_id
+      WHERE p.product_name LIKE ?
+    `;
     db.query(searchQuery, [`%${search}%`], (err, results) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send("Failed to search for products");
-        }
-        res.json(results);
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Failed to search for products");
+      }
+      res.json(results);
     });
-});
+  });
 
 export default router;
