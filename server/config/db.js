0ae50bcbfddecdc08da424f96dccb53d717c992d
Manas Patil <89078720+Manaspatil30@users.noreply.github.com>
2024-03-25 06:10:30 +0000

    deploymet changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,14 @@
 // const mysql = require('mysql')
-import mysql from 'mysql';
+import mysql from 'mysql2';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 const db = mysql.createPool({
-    host: 'localhost',
-    user: "root",
-    password: "",
-    database: "unikartdatabase"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 export default db;
